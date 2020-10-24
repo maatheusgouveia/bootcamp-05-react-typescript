b1106955d0b8db9048f70cea5fd417c6c565e9e5
maatheusgouveia <matheus_guitarplayer@hotmail.com>
2020-10-24 12:43:16 -0300

    conectando com a api do github

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,61 +1,69 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import { FiChevronRight } from "react-icons/fi";
+import api from "../../services/api";
 
 import { Form, Title, Repositories } from "./styles";
 
 import logoImg from "../../assets/logo.svg";
+import Repository from "../Repository";
+
+interface Repository {
+	full_name: string;
+	description: string;
+	owner: {
+		login: string;
+		avatar_url: string;
+	};
+}
 
 const Dashboard: React.FC = () => {
+	const [newRepo, setNewRepo] = useState("");
+	const [repositories, setRepositories] = useState<Repository[]>([]);
+
+	async function handleAddRepository(
+		e: FormEvent<HTMLFormElement>
+	): Promise<void> {
+		e.preventDefault();
+
+		const response = await api.get<Repository>(`repos/${newRepo}`);
+
+		const repository = response.data;
+
+		setRepositories([...repositories, repository]);
+
+		setNewRepo("");
+	}
+
 	return (
 		<>
 			<img src={logoImg} alt="Github Explorer" />
 			<Title>Explore repositorios no GitHub</Title>
 
-			<Form>
-				<input placeholder="Digite o nome do repositório" />
+			<Form onSubmit={handleAddRepository}>
+				<input
+					value={newRepo}
+					onChange={(e) => setNewRepo(e.target.value)}
+					placeholder="Digite o nome do repositório"
+				/>
 
 				<button type="submit">Pesquisar</button>
 			</Form>
 
 			<Repositories>
-				<a href="/teste">
-					<img
-						src="https://avatars1.githubusercontent.com/u/31319822?s=460&u=e95ebc1c99839c77810d75063539d9dcad2124a9&v=4"
-						alt=""
-					/>
+				{repositories.map((repository) => (
+					<a key={repository.full_name} href="/teste">
+						<img
+							src={repository.owner.avatar_url}
+							alt={repository.owner.login}
+						/>
 
-					<div>
-						<strong>maatheusgouveia/texuguito</strong>
-						<p>aisdbnasodbasidnbiasldnasl</p>
-					</div>
-					<FiChevronRight size={20} />
-				</a>
-
-				<a href="/teste">
-					<img
-						src="https://avatars1.githubusercontent.com/u/31319822?s=460&u=e95ebc1c99839c77810d75063539d9dcad2124a9&v=4"
-						alt=""
-					/>
-
-					<div>
-						<strong>maatheusgouveia/texuguito</strong>
-						<p>aisdbnasodbasidnbiasldnasl</p>
-					</div>
-					<FiChevronRight size={20} />
-				</a>
-
-				<a href="/teste">
-					<img
-						src="https://avatars1.githubusercontent.com/u/31319822?s=460&u=e95ebc1c99839c77810d75063539d9dcad2124a9&v=4"
-						alt=""
-					/>
-
-					<div>
-						<strong>maatheusgouveia/texuguito</strong>
-						<p>aisdbnasodbasidnbiasldnasl</p>
-					</div>
-					<FiChevronRight size={20} />
-				</a>
+						<div>
+							<strong>{repository.full_name}</strong>
+							<p>{repository.description}</p>
+						</div>
+						<FiChevronRight size={20} />
+					</a>
+				))}
 			</Repositories>
 		</>
 	);
