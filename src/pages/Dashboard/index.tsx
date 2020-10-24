49f27dfaf48b7370ebc957ece757e099d20a14ea
maatheusgouveia <matheus_guitarplayer@hotmail.com>
2020-10-23 23:40:39 -0300

    Listagem de repositórios

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,64 @@
 import React from "react";
-import { Title } from "./styles";
+import { FiChevronRight } from "react-icons/fi";
+
+import { Form, Title, Repositories } from "./styles";
+
+import logoImg from "../../assets/logo.svg";
 
 const Dashboard: React.FC = () => {
-	return <Title>Explore repositorios no GitHub</Title>;
+	return (
+		<>
+			<img src={logoImg} alt="Github Explorer" />
+			<Title>Explore repositorios no GitHub</Title>
+
+			<Form>
+				<input placeholder="Digite o nome do repositório" />
+
+				<button type="submit">Pesquisar</button>
+			</Form>
+
+			<Repositories>
+				<a href="/teste">
+					<img
+						src="https://avatars1.githubusercontent.com/u/31319822?s=460&u=e95ebc1c99839c77810d75063539d9dcad2124a9&v=4"
+						alt=""
+					/>
+
+					<div>
+						<strong>maatheusgouveia/texuguito</strong>
+						<p>aisdbnasodbasidnbiasldnasl</p>
+					</div>
+					<FiChevronRight size={20} />
+				</a>
+
+				<a href="/teste">
+					<img
+						src="https://avatars1.githubusercontent.com/u/31319822?s=460&u=e95ebc1c99839c77810d75063539d9dcad2124a9&v=4"
+						alt=""
+					/>
+
+					<div>
+						<strong>maatheusgouveia/texuguito</strong>
+						<p>aisdbnasodbasidnbiasldnasl</p>
+					</div>
+					<FiChevronRight size={20} />
+				</a>
+
+				<a href="/teste">
+					<img
+						src="https://avatars1.githubusercontent.com/u/31319822?s=460&u=e95ebc1c99839c77810d75063539d9dcad2124a9&v=4"
+						alt=""
+					/>
+
+					<div>
+						<strong>maatheusgouveia/texuguito</strong>
+						<p>aisdbnasodbasidnbiasldnasl</p>
+					</div>
+					<FiChevronRight size={20} />
+				</a>
+			</Repositories>
+		</>
+	);
 };
 
 export default Dashboard;
